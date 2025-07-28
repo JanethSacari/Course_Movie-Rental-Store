bfbc41fc14fb99a3d046a8998ec91cc2e9a93304
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-07-28 20:51:07 -0300

    Course: Movie Rental Store - Using error collector to improve tests structure.

--- src/test/java/com/repliforce/services/RentalServiceTest.java
+++ src/test/java/com/repliforce/services/RentalServiceTest.java
@@ -3,20 +3,22 @@ package com.repliforce.services;
 import com.repliforce.entities.Movie;
 import com.repliforce.entities.Rent;
 import com.repliforce.entities.User;
-import com.repliforce.utils.DateUtils;
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
 import static com.repliforce.utils.DateUtils.isSameDate;
 import static com.repliforce.utils.DateUtils.obtainDateWithDifferenceOfDays;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class RentalServiceTest {
+
+    @Rule
+    public ErrorCollector collector = new ErrorCollector();
 
     @Test
     public void testRentMovie() {
@@ -29,9 +31,8 @@ public class RentalServiceTest {
         Rent rent = rentalService.rentMovie(user, movie);
 
         // Verify the results
-        assertThat(rent.getPrice(), is(equalTo(10.0)));
-        assertThat(rent.getPrice(), is(not(6.0)));
-        assertThat(isSameDate(rent.getRentDate(), new Date()), is(true));
-        assertThat(isSameDate(rent.getReturnDate(), obtainDateWithDifferenceOfDays(1)), is(true));
+        collector.checkThat(rent.getPrice(), is(equalTo(10.0)));
+        collector.checkThat(isSameDate(rent.getRentDate(), new Date()), is(true));
+        collector.checkThat(isSameDate(rent.getReturnDate(), obtainDateWithDifferenceOfDays(1)), is(true));
     }
 }
